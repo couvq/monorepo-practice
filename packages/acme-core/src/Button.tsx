0e316354dc1152996bc78f503a327dcb5a4004fa
couvq <quentincouvelaire@gmail.com>
2023-08-19 09:55:32 -0500

    playing around with changing button

--- packages/acme-core/src/Button.tsx
+++ packages/acme-core/src/Button.tsx
@@ -5,7 +5,9 @@ export interface ButtonProps {
 }
 
 export function Button(props: ButtonProps) {
-  return <button>{props.children}</button>;
+  return <button style={{
+    backgroundColor: 'red'
+  }}>{props.children}</button>;
 }
 
 Button.displayName = "Button";
